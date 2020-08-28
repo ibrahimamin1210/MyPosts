5b37b47a416c8032955c174671e872880ed932d8
ibrahimamin12 <IbrahimAmin@smartera3s.com>
2020-08-28 11:24:00 +0200

    remove native query from repo

--- src/main/java/com/myposts/services/users/UserRepository.java
+++ src/main/java/com/myposts/services/users/UserRepository.java
@@ -1,11 +1,7 @@
 package com.myposts.services.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(
-            value = "SELECT * FROM system_users  WHERE user_name = ?1",
-            nativeQuery = true)
     User findByuserName(String userName);
 }
